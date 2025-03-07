39a3966079e776c176b4d34a75e119a0c0bfb04b
LuisCuadradoGuerra <lcuadradoguerra@gmail.com>
2025-03-07 09:54:47 +0100

    Recover JS and Fix pagination
    Fix Pagination: Next and Previous URL

---
+++ src/main/resources/static/js/buttonsFunctionalityAddToShoppingCart.js
@@ -0,0 +1,36 @@
+Array.from(document.querySelectorAll("button.add-to-cart-button"))
+    .forEach(button => {
+        button.addEventListener("click", (ev) => {
+            fetch(appPath + "api/v1", {
+                method: "POST",
+                body: JSON.stringify({
+                    productId: ev.target.getAttribute("data-product-id"),
+                    units: 1
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+                .then(value => {
+                    console.log("Ok")
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Product\nsuccessfully added ",
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                })
+
+                .catch(reason => {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "error",
+                        title: "Shit happens",
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                    console.log("Error")
+                })
+        })
+    });
